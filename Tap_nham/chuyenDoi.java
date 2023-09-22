4d700669c382370999f9bb9e98e424d702f6a4af
Hoang Phuc <tphuc9398@gmail.com>
2023-09-22 16:22:11 +0700

    Them chuyen doi nguyen to

--- Tap_nham/chuyenDoi.java
+++ Tap_nham/chuyenDoi.java
@@ -6,6 +6,28 @@ public class chuyenDoi {
         System.out.println("1: Kg to Pounds");
         System.out.println("2: C to F");
         System.out.println("3: Cm to index");
+        System.out.println("4: tim tong nguyen to");
+    }
+    public static boolean CheckPrime(int a){
+        if(a < 2){
+            return false;
+        }
+        for(int i = 2; i < a; i++){
+            if(a % i == 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public static void SumPrime(int a[]){
+        int sum = 0;
+        for(int i =0; i < a.length; i++){
+            if(CheckPrime(a[i])){
+                sum = sum + a[i];
+            }
+        }
+        System.out.print("Tong cac so nguyen to: "+ sum);
     }
     public static void main(String[] args) {
         int a;
@@ -43,6 +65,30 @@ public class chuyenDoi {
             System.out.println("Chon lai ko? [1/0]");
             ans = sc.nextInt();
             break;
+            case 4:
+
+            System.out.print("Nhap so luong: ");
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i = 0; i < n; i++){
+                System.out.print("Bien: ");
+                int x = sc.nextInt();
+                arr[i] = x;
+            }
+            System.out.println();
+            System.out.print("Mang: ");
+            for(int out : arr){
+                System.out.print(out+ " ");
+            }
+            System.out.println();
+            System.out.println("Check so nguyen to");
+            for(int out : arr){
+                if(CheckPrime(out)){
+                    System.out.print(out + " ");
+                }
+            }
+            System.out.println();       
+            SumPrime(arr);
         }
     }while(ans == 1);
     }
